bdfa6b66111e3d98a9e9e6f863a977dbae6d0f2b
Christine Doherty <christineadoherty@gmail.com>
2020-01-04 21:43:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,8 +57,11 @@ end
 
 def runner
   welcome
-  initial_round
-  hit?(initial_round)
-  
-end
+  total = initial_round
+    until total > 21
+      total = hit?(total)
+      display_card_total(total)
+    end
+    end_game(total)  
+  end
     
